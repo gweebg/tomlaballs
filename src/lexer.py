a2dcf8b7c7f70db9afd517ead9a8f395864457fb
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-05-09 12:07:11 +0100

    Datetime passing all tests.
    It was not supposed to accept no seconds.

--- src/lexer.py
+++ src/lexer.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 import ply.lex as lex
 import re
 
-from ply.lex import LexToken
+from ply.lex import LexToken, Lexer
 
-from exceptions import InvalidDatetimeFormat, IllegalCharacterException
+from exceptions import InvalidDatetimeFormat
 from utils import DateValidator, DateType
 
 
@@ -21,11 +23,13 @@ class TomlLexer:
 
     # Building the lexer.
     def build(self, **kwargs):
+
         self.lexer = lex.lex(module=self, **kwargs)
         self.lexer.array_num = 0
         self.lexer.inline_table_num = 0
 
     def print_toks(self, data):
+
         self.lexer.input(data)
 
         for tok in self.lexer:
@@ -37,33 +41,17 @@ class TomlLexer:
 
     tokens: tuple[str, ...] = (
 
-        'RSQBRACKET', 'LSQBRACKET',
-        'RBRACKET', 'LBRACKET',
-        'STRING_LITERAL',
-        'HEXADECIMAL',
-        'OFFSET_DATETIME',
-        'LOCAL_DATETIME',
-        'LOCAL_DATE',
-        'LOCAL_TIME',
-        'COMMENT',
-        'INTEGER',
-        'BINARY',
-        'EQUALS',
-        'STRING',
-        'OCTAL',
-        'COMMA',
-        'FLOAT',
-        'BOOL',
-        'DOT',
-        'BARE_KEY',
-        'STRING_KEY',
-        'STRING_LITERAL_KEY',
-        'MULTILINE_STRING',
-        'MULTILINE_STRING_LITERAL',
+        'RSQBRACKET', 'LSQBRACKET', 'RBRACKET', 'LBRACKET', 'STRING_LITERAL', 'HEXADECIMAL',
+        'OFFSET_DATETIME', 'LOCAL_DATETIME', 'LOCAL_DATE', 'LOCAL_TIME', 'COMMENT', 'INTEGER',
+        'BINARY', 'EQUALS', 'STRING', 'OCTAL', 'COMMA', 'FLOAT', 'BOOL', 'DOT', 'BARE_KEY',
+        'STRING_KEY', 'STRING_LITERAL_KEY', 'MULTILINE_STRING', 'MULTILINE_STRING_LITERAL',
 
     )
 
-    # Literal token definiton.
+    #############################
+    # Syntax token definitions. #
+    #############################
+
     t_ANY_COMMA = r'\,'
     t_ANY_DOT = r'\.'
 
@@ -105,7 +93,9 @@ class TomlLexer:
             t.lexer.begin('INITIAL')
         return t
 
-    # Misc
+    ########################################
+    # Error and ignored token definitions. #
+    ########################################
 
     t_ANY_ignore_COMMENT = r'\#.*'
 
@@ -116,7 +106,9 @@ class TomlLexer:
         # raise IllegalCharacterException(t.value[0])
         t.lexer.skip(1)
 
-    # Keys
+    ###########################
+    # Keys token definitions. #
+    ###########################
 
     t_BARE_KEY = r'[a-zA-Z0-9_-]+'
 
@@ -130,44 +122,51 @@ class TomlLexer:
         t.value = t.value.rstrip("'").lstrip("'")
         return t
 
-    # Values
-
-    # Strings
-
-    def t_VALUE_BOOL(self, t):
-        r'true|false'
-        if t.lexer.array_num == 0:
-            t.lexer.begin('INITIAL')
-        return t
+    #############################
+    # String token definitions. #
+    #############################
 
     def t_VALUE_STRING(self, t):
         r'"([^\\]|\\.)*?"'
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         t.value = t.value.rstrip('"').lstrip('"')
+
         return t
 
     def t_VALUE_STRING_LITERAL(self, t):
         r"'.*?'"
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         t.value = t.value.rstrip('"').lstrip('"')
+
         return t
 
     def t_VALUE_MULTILINE_STRING(self, t):
         r'"""([^\\]|\\(.|\n)|\n)*?"{3,5}'
+
         t.value = re.sub(r"\\(\n|\r\n)\s+", "", t.value)
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
     def t_VALUE_MULTILINE_STRING_LITERAL(self, t):
         r"'''(.|\n)*?'{3,5}"
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
-    # Date token definiton.
+    ##############################################
+    # Date, Time and Datetime token definitions. #
+    ##############################################
 
     def t_VALUE_OFFSET_DATETIME(self, t):
         r'\d{4}-\d{2}-\d{2}[Tt ](\d{2}:\d{2}(:\d{2}(\.\d+)?)?([Zz]|([-+]\d{2}:\d{2})))'
@@ -187,8 +186,6 @@ class TomlLexer:
     def t_VALUE_LOCAL_DATETIME(self, t):
         r'\d{4}-\d{2}-\d{2}[Tt ](\d{2}:\d{2}(:\d{2}(\.\d+)?)?)'
 
-        print("LOCAL DATETIME")
-
         t.value = t.value.upper()
 
         formatted_as: str = validate_date_format(t, DateType.LOCAL_DATETIME)
@@ -196,22 +193,21 @@ class TomlLexer:
 
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
     def t_VALUE_LOCAL_DATE(self, t):
         r'\d{4}-\d{2}-\d{2}'
 
-        print("LOCAL DATE")
-
         validate_date_format(t, DateType.LOCAL_DATE)
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
     def t_VALUE_LOCAL_TIME(self, t):
         r'(\d{2}:\d{2}(:\d{2}(\.\d+)?)?)'
 
-        print("LOCAL TIME")
         formatted_as: str = validate_date_format(t, DateType.LOCAL_TIME)
         t.value = DateValidator.normalize(t.value, formatted_as, DateType.LOCAL_TIME)
 
@@ -220,81 +216,71 @@ class TomlLexer:
 
         return t
 
-        # Number token definitions.
+    #############################
+    # Number token definitions. #
+    #############################
 
     def t_VALUE_HEXADECIMAL(self, t):
         r'0x[0-9A-Fa-f]([0-9A-Fa-f]|_[0-9A-Fa-f])*'
+
         t.value = int(t.value, 16)
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
     def t_VALUE_BINARY(self, t):
         r'0b[01]([01]|_[01])*'
+
         t.value = int(t.value, 2)
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
     def t_VALUE_OCTAL(self, t):
         r'0o[0-7]([0-7]|_[0-9])*'
+
         t.value = int(t.value, 8)
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
     def t_VALUE_FLOAT(self, t):
         r'((?=.*[eE\.])([-+]?(?:\d+(?:_\d+)*(?:\.\d*(?:_\d+)*)?|\.\d+(?:_\d+)*)(?:[eE][-+]?\d+(?:_\d+)*)?)((?:[eE\.][\d_+-]*))|((\+|\-)?nan)|((\+|\-)?inf))'
-        #r'((?=.*[eE\.])([-+]?(?:\d+(?:_\d+)*(?:\.\d*(?:_\d+)*)?|\.\d+(?:_\d+)*)(?:[eE][-+]?\d+(?:_\d+)*)?)((?:[eE\.]\d*))|((\+|\-)?nan)|((\+|\-)?inf))'
-        # r'((?=.*[eE\.])([-+]?(?:\d+(?:_\d+)*(?:\.\d*(?:_\d+)*)?|\.\d+(?:_\d+)*)(?:[eE][-+]?\d+(?:_\d+)*)?)((?:[eE\.].*))|((\+|\-)?nan)|((\+|\-)?inf))'
-        #((?=.*[eE\.])([-+]?(?:\d+(?:_\d+)*(?:\.\d*(?:_\d+)*)?|\.\d+(?:_\d+)*)(?:[eE][-+]?\d+(?:_\d+)*)?)|((\+|\-)?nan)|((\+|\-)?inf))
-        #(\+|-)?(\d(\d|_\d)*(\.\d(\d|_\d)*)?([eE](\+|-)?\d(\d|_\d)*)?|nan|inf)
+
         t.value = float(t.value)
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
         return t
 
     def t_VALUE_INTEGER(self, t):
         r'(\+|-)?\d(\d|_\d)*'
+
         t.value = int(t.value)
+
         if t.lexer.array_num == 0:
             t.lexer.begin('INITIAL')
+
+        return t
+
+    def t_VALUE_BOOL(self, t):
+        r'true|false'
+
+        if t.lexer.array_num == 0:
+            t.lexer.begin('INITIAL')
+
         return t
 
 
-# dob = 1979-05-27T07:32:00-08:00
-
-# data = """
-#
-## This is a TOML document
-#
-# title = "TOML Example"
-#
-# [owner]
-# "name" = "Tom Preston-Werner"
-#
-# [database]
-# enabled = true
-# ports = [ 8000, 8001, 8002 ]
-# data = [ ["delta", "phi"], "asd" ]
-# temp_targets = [ { yo = 2 }, "79.5", 72.0 ]
-#
-# [servers]
-#
-# [servers.alpha]
-# ip = "10.0.0.1"
-# role = "frontend"
-#
-# [servers.beta]
-# ip = "10.0.0.2"
-# role = "backend"
-#
-# """
-
 tLex = TomlLexer()
-
 tLex.build()
+
 tokens = tLex.tokens
 lexer = tLex.lexer
-
-# tLex.print_toks(data)
