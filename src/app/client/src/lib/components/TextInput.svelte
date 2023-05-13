f61fb99d887f862e1f92097d38df22b0b21bac04
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-05-13 17:27:50 +0100

    Fixed height of load file input.

--- src/app/client/src/lib/components/TextInput.svelte
+++ src/app/client/src/lib/components/TextInput.svelte
@@ -103,7 +103,7 @@
         margin: 3px;
         border-radius: 8px;
         border: 1px solid transparent;
-        padding: 0.6em 1.2em;
+        padding: 0.6em 1.2em 1.2em 5.2px;
         font-size: 1em;
         font-weight: 500;
         font-family: inherit;
